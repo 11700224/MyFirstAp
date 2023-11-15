50fd027bcd4d471164c9b553d9190ef0cb7420b1
11700224 <michael.vstr@gmail.com>
2023-11-15 11:44:45 +0100

    initial commit

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myfirstapp.ui.theme.MyFirstAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent()
+        Log.i("TAG", "onCreate")
+    }
+
+    private fun setContent() {
+        setContent {
+            setContentView(R.layout.activity_main)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyFirstAppTheme {
+        Greeting("Android")
+    }
+}
